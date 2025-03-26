8a85695e976416e4135cd88daf5e44d8c9527644
Sarah Österhed <sarah.osterhed@gmail.com>
2025-03-26 15:51:10 +0100

    added webhook

--- ecommerce-api/src/index.ts
+++ ecommerce-api/src/index.ts
@@ -1,13 +1,13 @@
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import { connectDB } from "./config/db";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 // Middleware
-const cors = require('cors');
 app.use(express.json())
 app.use(cors())
 
@@ -24,24 +24,61 @@ app.use('/order-items', orderItemRouter)
 
 
 
-app.post('/stripe/create-checkout-session', async (req: Request, res: Response) => {
+app.post('/stripe/create-checkout-session-embedded', async (req: Request, res: Response) => {
   const lineItems: ICheckoutLineItem = req.body.lineItems;
   console.log(lineItems)
   const clientReferenceId = req.body.clientReferenceId;
   console.log(clientReferenceId)
 
   const session = await stripe.checkout.sessions.create({
-    line_items: lineItems,
+    line_items: [{
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: 'T-shirt',
+          images: ["image-url.com"]
+        },
+        unit_amount: 2000,
+      },
+      quantity: 1,
+    }],
     mode: 'payment',
-    success_url: 'http://localhost:5173/order-confirmation',
-    cancel_url: 'http://localhost:5173/checkout',
-    client_reference_id: clientReferenceId
+    ui_mode: 'embedded',
+    // success_url: 'http://localhost:5173/order-confirmation',
+    client_reference_id: clientReferenceId,
+    return_url: 'http://localhost:5173/order-confirmation?session_id={CHECKOUT_SESSION_ID}'
   });
 
-  res.json({ checkoutUrl: session.url, sessionId: session.id, clientReferenceId: session.client_reference_id, session: session })
+  // res.json({ checkoutUrl: session.url, sessionId: session.id, clientReferenceId: session.client_reference_id, session: session })
 
-  // res.redirect(303, session.url);
+  res.send({ clientSecret: session.client_secret });
+
 });
+
+
+
+app.post("/stripe/webhook", (req: Request, res: Response) => {
+  const event = req.body;
+
+  //Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      const checkoutSession = event.type.object
+      console.log(event.type.object)
+      // Then define and call a method to handle the successful attachment of a PaymentMethod.
+      // handlePaymentMethodAttached(paymentMethod);
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a response to acknowledge receipt of the event
+  res.json({ received: true });
+
+
+})
+
 
 // Attempt to connect to the database
 connectDB()
