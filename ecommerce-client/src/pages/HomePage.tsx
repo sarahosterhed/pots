93656120cc5c3f8ecbc6bc682e57a2f7c72f54ec
Sarah Österhed <sarah.osterhed@gmail.com>
2025-04-10 22:58:12 +0200

    feat: update HomePage with SEO metadata and add favicon and logo SVGs

--- ecommerce-client/src/pages/HomePage.tsx
+++ ecommerce-client/src/pages/HomePage.tsx
@@ -3,19 +3,11 @@ import { DisplayProducts } from "../components/Start/DisplayProducts"
 import "../styles/pages/HomePage.css"
 import { IndexedItem } from "../types/IndexedItem"
 import { SearchBar } from "../components/Start/SearchBar"
-import { Helmet } from "react-helmet-async";
 
 export const HomePage = () => {
     const [filteredProducts, setFilteredProducts] = useState<IndexedItem[]>([]);
     return (
         <>
-            <Helmet >
-                <title>Home | Pots</title>
-                <meta name="google-site-verification" content="V2tsYfrsOglRaB88LJ17QZ4EXtwie1sWWCB74CQJFv8" />
-                <meta property="og:title" content="Pots | Grow your vibe" />
-                <meta name="description" content="Discover beautiful and rare indoor plants for your home. Shop now at Pots – your green space begins here." />
-                <meta property="og:type" content="website" />
-            </Helmet>
             <SearchBar setFilteredProducts={setFilteredProducts} />
             <DisplayProducts filteredProducts={filteredProducts} />
         </>
